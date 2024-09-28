d725552ec8291e4b21ce36a8a048e89193bbac57
alireza esmaeili <103310265+Alirezaesmaeili1@users.noreply.github.com>
2024-09-28 08:22:22 +0330

    adding touchMannager and playerMovement

---
+++ runnergame/Assets/Scripts/TouchMannager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour
+{
+    private Vector3 touchPosition;
+    private Touch touch;
+    private void Touch()
+    {
+        if (Input.touchCount != 1)
+        {
+            return;
+        }
+        touch = Input.touches[0];
+        touchPosition = touch.position;
+
+        if (touch.phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+            
+            }
+        }
+        if (touch.phase == TouchPhase.Moved)
+        {
+
+        }
+        if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+               
+            }
+        }
+    }
+    void Update()
+    {
+        Touch();
+    }
+}
