e87ae68e7c50c9bb352a6facb1594afc2c3d37f2
MyBrainIsDead <hooman14789632@gmail.com>
2024-09-29 22:06:51 +0330

    New Chunk and Obstacle Loader

--- runnergame/Assets/Script/SectionTrigger.cs
+++ runnergame/Assets/Script/SectionTrigger.cs
@@ -1,16 +1,21 @@
+using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SectionTrigger : MonoBehaviour
 {
     [SerializeField]
+    List<GameObject> sectionChunks;
+
     GameObject sectionChunk;
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("ChunkTrigger"))
         {
+            sectionChunk = sectionChunks.OrderBy(x => System.Guid.NewGuid()).FirstOrDefault();
             Instantiate(sectionChunk,new Vector3(-10, 0, 0),Quaternion.identity);
         }
     }
